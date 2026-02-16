c30c0545c8157dc0c9fd16c86ebf33d8848ce91e
Codinkage <husseinmubarak500@gmail.com>
2026-02-16 17:27:42 +0100

    plugin(database): Integration of scheduler database

---
+++ src/plugins/database.ts
@@ -0,0 +1,15 @@
+import { FastifyPluginAsync } from "fastify";
+import fp from "fastify-plugin";
+import postgres from "postgres";
+
+const database: FastifyPluginAsync = async (fastify, opts) => {
+  const sql = postgres(process.env.DATABASE_URL!, { max: 20 });
+
+  fastify.decorate("sql", sql);
+
+  fastify.addHook("onClose", async () => {
+    await sql.end();
+  });
+};
+
+export default fp(database);
--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -1,20 +1,17 @@
-import fp from 'fastify-plugin'
+import fp from "fastify-plugin";
+import postgres from "postgres";
 
-export interface SupportPluginOptions {
-  // Specify Support plugin options here
-}
+export interface SupportPluginOptions {}
 
-// The use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
-  })
-})
+  fastify.decorate("someSupport", function () {
+    return "hugs";
+  });
+});
 
-// When using .decorate you have to specify added properties for Typescript
-declare module 'fastify' {
+declare module "fastify" {
   export interface FastifyInstance {
     someSupport(): string;
+    sql: postgres.Sql<{}>;
   }
 }
