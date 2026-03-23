3b8aba0bed66336812a1dc1b67ae83ef9abded8f
Codinkage <husseinmubarak500@gmail.com>
2026-03-23 11:21:11 +0100

    ...

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ closeWithGrace(
 );
 
 fastify.listen(
-  { host: "0.0.0.0", port: Number(process.env.PORT) || 3000 },
+  { host: "0.0.0.0", port: Number(process.env.PORT) || 8000 },
   function (err, address) {
     if (err) {
       fastify.log.error(err);
