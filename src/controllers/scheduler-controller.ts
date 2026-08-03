5b9eaa803c357e6350d18481c3df39a3ded87540
Codinkage <husseinmubarak500@gmail.com>
2026-08-03 22:25:17 +0100

    fix: minor deployment issues fixed

--- src/controllers/scheduler-controller.ts
+++ src/controllers/scheduler-controller.ts
@@ -1,8 +1,7 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { StatusCodes } from "http-status-codes";
 import postgres from "postgres";
-import EmailService from "../services/email";
-import { Invoice, UserData } from "../types/invoice";
+import { Invoice } from "../types/invoice";
 
 export class SchedulerController {
   private sql: postgres.Sql<{}>;
@@ -13,22 +12,24 @@ export class SchedulerController {
 
   async fetchUnpaidInvoices(request: FastifyRequest, reply: FastifyReply) {
     const invoiceId = "e95eae3a-7522-4094-83c1-71ac78856bd0";
-    const userId = "e8f00ded-31f0-4b43-a471-037a51608c4b";
 
     const invoices = await this.sql`
        select * from invoices 
       where id=${invoiceId} 
     `;  
 
-    const users = await this.sql`
-      select * from users 
-      where id=${userId}
-    `; 
-
     const invoice = invoices[0] as Invoice;
-    const user = users[0] as UserData;
 
-    await EmailService.sendOverdueInvoiceMail(invoice, user);
+    // Code below commented out for testing purposes. 
+
+    // const users = await this.sql`
+    //   select * from users 
+    //   where id=${userId}
+    // `; 
+
+    // const user = users[0] as UserData;
+
+    // await EmailService.sendOverdueInvoiceMail(invoice, user);
 
     return reply
       .status(StatusCodes.OK)
