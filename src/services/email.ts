28d073aab7b38f45549492cc14a90eea2b55ac7b
Codinkage <husseinmubarak500@gmail.com>
2026-08-30 20:41:08 +0100

    chore(email): Updated reset password link url

--- src/services/email.ts
+++ src/services/email.ts
@@ -73,7 +73,7 @@ class EmailService {
   }
 
   public static async sendResetVerificationLink(payload: UserData, token: string) {
-    const reset_link = `https://billgig.app/reset-password?token=${token}`; 
+    const reset_link = `https://dash.billgig.app/reset-password?token=${token}`; 
 
     const { data: _data, error: _error } = await this.resend.emails.send({
       from: VERIFICATION_EMAIL_FROM,
